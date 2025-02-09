ff0a0dd6193ccc47532e8d51b40f890a8672bb81
Haruki1090 <haruharu.com.109607@gmail.com>
2025-02-09 16:54:21 +0900

    uid

--- lib/viewmodels/task_viewmodel.dart
+++ lib/viewmodels/task_viewmodel.dart
@@ -1,17 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gtd_task_manager/models/notion_account.dart';
 import 'package:gtd_task_manager/models/task.dart';
 import 'package:gtd_task_manager/repositories/task_repository.dart';
 import 'package:gtd_task_manager/services/notion_service.dart';
+import 'package:gtd_task_manager/viewmodels/auth_viewmodel.dart';
 
 final taskRepositoryProvider =
     Provider<TaskRepository>((ref) => TaskRepository());
 
 final tasksProvider = StreamProvider.autoDispose<List<Task>>((ref) {
-  const userId = "currentUserId"; // ※認証済みユーザーのIDに置き換える
+  var userId = ref.watch(authStateProvider).data?.value?.uid;
   final taskRepository = ref.watch(taskRepositoryProvider);
   return taskRepository.getTasks(userId);
 });
+
+extension on AsyncValue<User?> {
+  get data => null;
+}
 
 final taskViewModelProvider = Provider<TaskViewModel>((ref) {
   final taskRepository = ref.watch(taskRepositoryProvider);
