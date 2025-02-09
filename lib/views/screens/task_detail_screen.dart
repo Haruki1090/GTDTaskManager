42543b2bd15746cf49d9642ec899ea3c1e2495b9
Haruki1090 <haruharu.com.109607@gmail.com>
2025-02-10 00:07:00 +0900

    タスク詳細画面の実装

---
+++ lib/views/screens/task_detail_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class TaskDetailScreen extends ConsumerStatefulWidget {
+  final String taskId;
+  const TaskDetailScreen({super.key, required this.taskId});
+
+  @override
+  ConsumerState<TaskDetailScreen> createState() => _TaskDetailScreenState();
+}
+
+class _TaskDetailScreenState extends ConsumerState<TaskDetailScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('タスク詳細'),
+      ),
+      body: Center(
+        child: Text('Task ID: ${widget.taskId}'),
+      ),
+    );
+  }
