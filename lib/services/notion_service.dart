a366e1d8f63efcac7d52c173b7bd05b51a854a60
Haruki1090 <haruharu.com.109607@gmail.com>
2025-02-09 22:37:30 +0900

    Add a method to add tasks to Notion

--- lib/services/notion_service.dart
+++ lib/services/notion_service.dart
@@ -71,4 +71,68 @@ class NotionService {
       }
     }
   }
+
+  // ユーザーのNotionAccount情報を用いてタスクを追加する
+  static Future<void> addTask(NotionAccount account, Task task) async {
+    const notionVersion = '2022-06-28';
+    final url = Uri.parse('https://api.notion.com/v1/pages');
+
+    final payload = {
+      "parent": {"database_id": account.databaseId},
+      "properties": {
+        "Name": {
+          "title": [
+            {
+              "text": {"content": task.title}
+            }
+          ]
+        },
+        "Status": {
+          "select": {"name": task.status.toString().split('.').last}
+        },
+        if (task.dueDate != null)
+          "Due Date": {
+            "date": {"start": task.dueDate!.toIso8601String()}
+          },
+        if (task.project != null)
+          "Project": {
+            "rich_text": [
+              {
+                "text": {"content": task.project}
+              }
+            ]
+          },
+        if (task.context != null)
+          "Context": {
+            "rich_text": [
+              {
+                "text": {"content": task.context}
+              }
+            ]
+          },
+        if (task.context != null)
+          "Description": {
+            "rich_text": [
+              {
+                "text": {"content": task.description}
+              }
+            ]
+          },
+      },
+    };
+
+    final response = await http.post(
+      url,
+      headers: {
+        "Authorization": "Bearer ${account.accessToken}",
+        "Notion-Version": notionVersion,
+        "Content-Type": "application/json",
+      },
+      body: jsonEncode(payload),
+    );
+
+    if (response.statusCode != 200 && response.statusCode != 201) {
+      throw Exception('タスク ${task.id} の追加に失敗: ${response.body}');
+    }
+  }
 }
