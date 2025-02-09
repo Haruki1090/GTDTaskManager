098fa3bc5d107d0d6e0b0fef13ed932f6c160f15
Haruki1090 <haruharu.com.109607@gmail.com>
2025-02-09 19:10:21 +0900

    環境変数部分を削除

--- lib/services/firebase_initializer.dart
+++ lib/services/firebase_initializer.dart
@@ -1,14 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class FirebaseInitializer {
   static Future<FirebaseApp> initializeFirebase() async {
-    return Firebase.initializeApp(
-        options: FirebaseOptions(
-      apiKey: dotenv.env['FIREBASE_API_KEY']!,
-      appId: dotenv.env['FIREBASE_APP_ID']!,
-      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
-      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
-    ));
+    return Firebase.initializeApp();
   }
 }
