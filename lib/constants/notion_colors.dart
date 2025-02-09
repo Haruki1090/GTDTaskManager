f628014a68f1f2815622a234263af86c68ddf349
Haruki1090 <haruharu.com.109607@gmail.com>
2025-02-09 13:31:45 +0900

    カラー設定

---
+++ lib/constants/notion_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final Color redColor = const Color(0xFFD44C47);
+final Color blueColor = const Color(0xFF337EA9);
+final Color greenColor = const Color(0xFF448361);
+final Color yellowColor = const Color(0xFFCB912F);
+final Color orangeColor = const Color(0xFFD9730D);
+final Color brownColor = const Color(0xFF9F6B53);
+final Color greyColor = const Color(0xFF787774);
+final Color purpleColor = const Color(0xFF9065B0);
+final Color pinkColor = const Color(0xFFC14C8A);
