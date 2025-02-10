910a5a595092607c0e9d080d8744826ab7d4f778
Haruki1090 <haruharu.com.109607@gmail.com>
2025-02-10 15:05:10 +0900

    Drawerに一部切り出し

--- lib/views/widgets/glass_app_bar.dart
+++ lib/views/widgets/glass_app_bar.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gtd_task_manager/constants/notion_colors.dart';
 import 'package:gtd_task_manager/viewmodels/auth_viewmodel.dart';
-import 'package:gtd_task_manager/views/screens/account_settings_screen.dart';
-import 'package:gtd_task_manager/views/screens/settings_screen.dart';
 
 class GlassAppBar extends ConsumerWidget implements PreferredSizeWidget {
   final double blur;
@@ -31,71 +29,11 @@ class GlassAppBar extends ConsumerWidget implements PreferredSizeWidget {
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: blur, sigmaY: blur),
         child: AppBar(
-          title: Text('ALL TASKS'),
+          title: Text('ALL TASKS',
+              style: TextStyle(
+                  color: blueColor, fontSize: 24, fontWeight: FontWeight.bold)),
           backgroundColor: backgroundColor.withOpacity(opacity),
           elevation: 0,
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.settings),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const SettingsScreen()),
-                );
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.face),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (_) => const AccountSettingsScreen()),
-                );
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.logout),
-              onPressed: () async {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      backgroundColor: Colors.white,
-                      title: const Text('ログアウトしますか？',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(fontSize: 20)),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child:
-                              Text('キャンセル', style: TextStyle(color: redColor)),
-                        ),
-                        Container(
-                          width: 100,
-                          height: 40,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            color: blueColor,
-                          ),
-                          child: TextButton(
-                            onPressed: () async {
-                              Navigator.pop(context);
-                              await authVM.signOut();
-                            },
-                            child: const Text('ログアウト',
-                                style: TextStyle(color: Colors.white)),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-            ),
-          ],
         ),
       ),
     );
