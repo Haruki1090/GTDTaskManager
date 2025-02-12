4bc7016cd00296d7b0282582ef957301c25ec624
Haruki1090 <haruharu.com.109607@gmail.com>
2025-02-12 09:50:16 +0900

    リスト化

--- lib/constants/notion_colors.dart
+++ lib/constants/notion_colors.dart
@@ -9,3 +9,15 @@ final Color brownColor = const Color(0xFF9F6B53);
 final Color greyColor = const Color(0xFF787774);
 final Color purpleColor = const Color(0xFF9065B0);
 final Color pinkColor = const Color(0xFFC14C8A);
+
+const List<int> notionColors = [
+  0xFFD44C47, // Red
+  0xFF337EA9, // Blue
+  0xFF448361, // Green
+  0xFFCB912F, // Yellow
+  0xFFD9730D, // Orange
+  0xFF9F6B53, // Brown
+  0xFF787774, // Grey
+  0xFF9065B0, // Purple
+  0xFFC14C8A, // Pink
+];
