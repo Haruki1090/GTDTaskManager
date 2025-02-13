5b10ff5e0f86fd3e5e7d3b007c46863e069a9459
Haruki1090 <haruharu.com.109607@gmail.com>
2025-02-13 10:40:37 +0900

    bug

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -308,7 +308,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   }
 
   // タスク詳細編集用のモーダルボトムシート
-  // todo: [bug]カラーが変更されない
+  // todo: [bug]updateされない
   Future<void> _showTaskDetailModal(Task task) async {
     _taskTitleController.text = task.title;
     _taskDescController.text = task.description ?? '';
